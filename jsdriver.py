76042b8e3ec3f917700a89e2a5dd03aed56e0da5
Mesar Hameed <mhameed@src.gnome.org>
2013-05-03 14:47:30 +0200

    now extending thread, much cleaner interface to the outside world.

--- jsdriver.py
+++ jsdriver.py
@@ -1,6 +1,6 @@
-import threading
 from struct import unpack
 import time
+import threading
 import logging
 import plumbum
 from plumbum.cmd import xte
@@ -10,12 +10,24 @@ JS_EVENT_BUTTON = 0x01    # button pressed/released
 JS_EVENT_AXIS = 0x02    # joystick moved
 JS_EVENT_INIT = 0x80    # initial state of device
 
-class Driver(object):
+class Driver(threading.Thread):
 
+    def __init__(self, device, logfile='motion.log', loglevel=logging.WARNING, *args, **kwargs):
+        super(Driver, self).__init__(*args, **kwargs)
+        self.device = device
+        self._stop = threading.Event()
+        logging.basicConfig(filename=logfile,
+            level=loglevel,
+            format='%(asctime)s - %(levelname)s - %(message)s')
 
+    def stop(self):
+        self._stop.set()
 
-    def processMotionData(self):
-        while True:
+    def run(self):
+        logging.info("motion driver: starting.")
+        self.f = open(self.device, 'rb')
+        while not self._stop.isSet():
+
             etime = unpack('I', self.f.read(4))[0]
             evalue = unpack('h', self.f.read(2))[0]
             etype = unpack('B', self.f.read(1))[0]
@@ -37,34 +49,5 @@ class Driver(object):
             method(evalue)
             time.sleep(0)
 
-    def __init__(self, keymap, device, boundary=50, keyDelay=0.35, logfile='motion.log', loglevel=logging.WARNING):
-        self.f = None
-        self.x=0
-        self.b1state=0
-        self.b2state=0
-        self.b3state=0
-        self.keyDelay = keyDelay
-        self.boundary = boundary
-        self.device = device
-        self.keymap = keymap
-        logging.basicConfig(filename=logfile,
-            level=loglevel,
-            format='%(asctime)s - %(levelname)s - %(message)s')
-
-    def start(self):
-        logging.info("motion driver: starting.")
-        self.f = open(self.device, 'rb')
-
-        t1 = threading.Thread(target=self.processMotionData)
-        t1.daemon = True
-        t1.start()
-        try:
-            while True:
-                time.sleep(self.keyDelay)
-
-        except KeyboardInterrupt:
-            pass
-        except Exception as err:
-            raise 
-        #self.f.close()
+        self.f.close()
         logging.info("motion driver: shutting down.")
