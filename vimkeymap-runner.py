3330ade2443ddc04ec936bcbd4265228457da59a
Mesar Hameed <mhameed@src.gnome.org>
2013-05-10 22:08:44 +0200

    Add logging to vimkeymap-runner to reflect new logging structure.

--- vimkeymap-runner.py
+++ vimkeymap-runner.py
@@ -1,5 +1,14 @@
 import time
+import logging
 from vimkeymap import VimKeymap
+
+logger = logging.getLogger('motion')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('motion.log')
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 
 d = VimKeymap()
 d.start()
